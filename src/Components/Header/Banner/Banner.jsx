cc77b47f5eeb8ddb8ff82582a3f6819de986ef93
aminsk12 <aminskeh786@gmail.com>
2023-09-27 19:58:17 +0530

    banner section complit

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
                 <div className="hero-overlay bg-white bg-opacity-80"></div>
                 <div className="hero-content text-center text-neutral-content">
                 <div className="">
-                        <h1 className=" text-2xl text-black ">I Grow By Helping People In Need</h1>
+                        <h1 className=" text-2xl text-black git">I Grow By Helping People In Need</h1>
                         <input className=" py-3 rounded-l-lg lg:pr-52 lg:pl-8 " type="text" placeholder="Search here...." />
-                        <button className=" absolute  bg-[#FF444A] py-3 font-bold rounded-r-lg px-5">Search</button>
+                        
                     </div>
                 </div>
             </div>
